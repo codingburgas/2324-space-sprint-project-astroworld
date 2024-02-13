433764f8096beed95123d29397811653d4e904a2
Stoyan Hristov <SPHristov22@codingburgas.bg>
2024-02-13 21:03:10 +0200

    Set background picture

--- game/Project/game.cpp
+++ game/Project/game.cpp
@@ -1,7 +1,9 @@
 #include "game.h"
+
 #define MAX_COLUMNS 20
 void game()
 {
+    Texture2D bgPhoto = LoadTexture("../images/spacePhotoBG.png");
     const int screenWidth = 1920;
     const int screenHeight = 975;
     Camera camera = { 0 };
@@ -9,11 +11,10 @@ void game()
     camera.target = { 0.0f, 2.0f, 0.0f };      
     camera.up = { 0.0f, 1.0f, 0.0f };          
     camera.fovy = 60.0f;                       
-    camera.projection = CAMERA_PERSPECTIVE;    
- 
+    camera.projection = CAMERA_PERSPECTIVE;   
+
     int cameraMode = CAMERA_FIRST_PERSON;
- 
- 
+    
     float heights[MAX_COLUMNS] = { 0 };
     Vector3 Pebblepositions[MAX_COLUMNS] = { 0 };
 
@@ -21,36 +22,31 @@ void game()
     for (int i = 0; i < MAX_COLUMNS; i++)
     {
         heights[i] = (float)GetRandomValue(0.9, 1.0);
-        Pebblepositions[i] = { (float)GetRandomValue(-29,29), heights[i] / 2.0f, (float)GetRandomValue(-25, 25) };
+        Pebblepositions[i] = { (float)GetRandomValue(-80,80), heights[i] / 2.0f, (float)GetRandomValue(-25, 25) };
     }
  
     DisableCursor();                  
- 
-    SetTargetFPS(60);                 
+    SetTargetFPS(165);                 
     while (!WindowShouldClose())      
     {
 
-        UpdateCamera(&camera, cameraMode);                    
+        UpdateCamera(&camera, cameraMode);
+
         BeginDrawing();
- 
-        ClearBackground(ORANGE);
- 
+
+        ClearBackground(RAYWHITE);
+
+        DrawTexture(bgPhoto, 0, 0, WHITE);
         BeginMode3D(camera);
- 
-        DrawPlane( { 0.0f, 0.0f, 0.0f },{ 64.0f, 64.0f }, RED);
+
+        DrawPlane( { 0.0f, 0.0f, 0.0f },{ 164.0f, 164.0f }, RED);
         for (int i = 0; i < MAX_COLUMNS; i++)
         {
-            DrawCube(Pebblepositions[i], 0.5f, 0.9f, 0.5f, LIGHTGRAY);
+            DrawCube(Pebblepositions[i], 1.5f, { 0.9f }, 1.5f, LIGHTGRAY);
         }
- 
-       
-        if (cameraMode == CAMERA_THIRD_PERSON)
-        {
-            DrawCube(camera.target, 0.5f, 0.5f, 0.5f, PURPLE);
-        }
- 
+        
         EndMode3D();
- 
+      
         EndDrawing();
     }
  
