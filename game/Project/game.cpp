cd9e7e348dcaba176c16dfea129e13ca4ed0d2d6
ZVYankova22 <ZVYankova22@codingburgas.bg>
2024-02-16 23:42:03 +0200

    Display spacecraft parts on map and add gitignore

--- game/Project/game.cpp
+++ game/Project/game.cpp
@@ -1,41 +1,69 @@
 #include "game.h"
 
 #define ROCKS 200
-#define PARTS 6
 void game()
 {
-    Texture2D bgPhoto = LoadTexture("../images/bgPhoto.png");
-    Texture2D firstPart = LoadTexture ("../images/spacecraft/firstPart.png");
-    Texture2D secondPart = LoadTexture ("../images/spacecraft/secondPart.png");
-    Texture2D thirdPart = LoadTexture ("../images/spacecraft/thirdPart.png");
-    Texture2D fourthPart = LoadTexture ("../images/spacecraft/fourthPart.png");
-    Texture2D fifthPart = LoadTexture ("../images/spacecraft/fifthPart.png");
-    Texture2D sixthPart = LoadTexture ("../images/spacecraft/sixthPart.png");
-
+    //Screen's size
     const int screenWidth = 1920;
     const int screenHeight = 975;
-    Camera camera = { 0 };
-    camera.position = { 0.0f, 2.0f, 4.0f };    
-    camera.target = { 0.0f, 2.0f, 0.0f };      
-    camera.up = { 0.0f, 1.0f, 0.0f };          
-    camera.fovy = 60.0f;                       
-    camera.projection = CAMERA_PERSPECTIVE;   
 
-    int cameraMode = CAMERA_FIRST_PERSON;
-    
+    //Game's background
+    Texture2D bgPhoto = LoadTexture("../images/bgPhoto.png");
+
+
+    //Objects
     float heights[ROCKS] = { 0 };
     Vector3 rockPositions[ROCKS] = { 0 };
-    Vector3 partsPositions[PARTS] = { 0 };
- 
+
+
+    //First spacecraft part object
+    Model firstPartobj = LoadModel("../images/spacecraft/objects/firstPart.obj");
+    Texture2D firstPartTexture = LoadTexture("..images/spacecraft/firstPart.png");
+    Vector3 positionOne = { 2.5f,0.5f,3.8f };
+
+    //Second spacecraft part object
+    Model secondPartobj = LoadModel("../images/spacecraft/objects/secondPart.obj");
+    Texture2D secondPartTexture = LoadTexture("..images/spacecraft/secondPart.png");
+    Vector3 positionTwo = { -4.0f,0.5f,-4.7f };
+
+    //Third spacecraft part object 
+    Model thirdPartobj = LoadModel("../images/spacecraft/objects/thirdPart.obj");
+    Texture2D thirdPartTexture = LoadTexture("..images/spacecraft/thirdPart.png");
+    Vector3 positionThree = { -6.0f,0.5f,4.9f };
+
+    //Fourth spacecraft part object
+    Model fourthPartobj = LoadModel("../images/spacecraft/objects/fourthPart.obj");
+    Texture2D fourthPartTexture = LoadTexture("..images/spacecraft/fourthPart.png");
+    Vector3 positionFour = { 4.6f,0.5f,-5.4f };
+
+    //Fifth spacecraft part object
+    Model fifthPartobj = LoadModel("../images/spacecraft/objects/fifthPart.obj");
+    Texture2D fifthPartTexture = LoadTexture("..images/spacecraft/fifthPart.png");
+    Vector3 positionFive = { 3.0f,0.5f,-4.0f };
+
+    //Sixth spacecraft part object
+    Model sixthPartobj = LoadModel("../images/spacecraft/objects/sixthPart.obj");
+    Texture2D sixthPartTexture = LoadTexture("..images/spacecraft/sixthPart.png");
+    Vector3 positionSix = { 5.6f,0.5f,-2.0f };
+
+    //Variables for the camera
+    Camera camera = { 0 };
+    camera.position = { 0.0f, 2.0f, 4.0f };
+    camera.target = { 0.0f, 2.0f, 0.0f };
+    camera.up = { 0.0f, 1.0f, 0.0f };
+    camera.fovy = 60.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+    int cameraMode = CAMERA_FIRST_PERSON;
+
+    //Defines rocks' positions
     for (int i = 0; i < ROCKS; i++)
     {
         heights[i] = (float)GetRandomValue(0.9, 1.0);
         rockPositions[i] = { (float)GetRandomValue(-110,110), heights[i] / 2.0f, (float)GetRandomValue(-100, 100) };
     }
- 
-    DisableCursor();                  
-    SetTargetFPS(165);                 
-    while (!WindowShouldClose())      
+    DisableCursor();
+    SetTargetFPS(165);
+    while (!WindowShouldClose())
     {
 
         UpdateCamera(&camera, cameraMode);
@@ -46,27 +74,25 @@ void game()
 
         DrawTexture(bgPhoto, 0, 0, WHITE);
 
-        DrawTexture(firstPart, 10, 10, WHITE);
-        DrawTexture(secondPart, 20, 20, WHITE);
-        DrawTexture(thirdPart, 30, 30, WHITE);
-        DrawTexture(fourthPart, 40, 40, WHITE);
-        DrawTexture(fifthPart, 50, 50, WHITE);
-        DrawTexture(sixthPart, 60, 60, WHITE);
-
         BeginMode3D(camera);
 
-        DrawPlane( { 0.0f, 0.0f, 0.0f },{ 250.0f, 250.0f }, { 192, 96, 0, 250});
+        //Draw ground
+        DrawPlane({ 0.0f, 0.0f, 0.0f }, { 250.0f, 250.0f }, { 192, 96, 0, 250 });
+
         for (int i = 0; i < ROCKS; i++)
         {
-            DrawCube(rockPositions[i], 1.5f, { 0.9f }, 1.5f, { 138, 69, 0, 250});
+            DrawCube(rockPositions[i], 1.5f, { 0.9f }, 1.5f, { 138, 69, 0, 250 });
         }
-        
+        DrawModel(firstPartobj, positionOne, 0.025f, WHITE);
+        DrawModel(secondPartobj, positionTwo, 0.025f, WHITE);
+        DrawModel(thirdPartobj, positionThree, 0.025f, WHITE);
+        DrawModel(fourthPartobj, positionFour, 0.025f, WHITE);
+        DrawModel(fifthPartobj, positionFive, 0.025f, WHITE);
+        DrawModel(sixthPartobj, positionSix, 0.025f, WHITE);
         EndMode3D();
 
         EndDrawing();
     }
- 
-   
     SetExitKey(KEY_ESCAPE);
     EnableCursor();
 }
