a5977504fa4a34aabaeaa7a0d282d345d03c65fd
DHNoneva22 <132429628+DHNoneva22@users.noreply.github.com>
2024-02-23 23:16:41 +0200

    Change the menu's background

--- game/Project/menu.cpp
+++ game/Project/menu.cpp
@@ -8,9 +8,9 @@ void mainMenu()
     InitWindow(screenWidth, screenHeight, "ASTROWORLD");
     bool exitGame = WindowShouldClose();
     Texture2D menuBG = LoadTexture("../images/mainMenuBg.png");
-    const Rectangle startButton = { (screenWidth / 2) - 500, (screenHeight / 2) - 50, 270, 90 };
-    const Rectangle rulesButton = { (screenWidth / 2) + 300, (screenHeight / 2) - 50, 270, 90 };
-    const Rectangle exitButton = { (screenWidth / 2) - 65, (screenHeight / 2) + 380, 270, 90 };
+    const Rectangle startButton = { (screenWidth / 2) + 340, (screenHeight / 2) - 15, 270, 90 };
+    const Rectangle rulesButton = { (screenWidth / 2) + 340, (screenHeight / 2) + 140, 270, 90 };
+    const Rectangle exitButton = { (screenWidth / 2) + 340, (screenHeight / 2) + 295, 270, 90 };
 
     while (!exitGame)
     {
@@ -20,24 +20,24 @@ void mainMenu()
         Vector2 mousePosition = GetMousePosition();
         bool isMouseOverButtonStart = CheckCollisionPointRec(mousePosition, startButton);
         DrawTexture(menuBG, 0, 0, WHITE);
-        DrawRectangleRec(startButton, (isMouseOverButtonStart ? ORANGE : GRAY));
-        DrawText("Start", screenWidth / 2 - 440, screenHeight / 2 - 25, 50, BLACK);
+        DrawRectangleRec(startButton, (isMouseOverButtonStart ? PURPLE : GRAY));
+        DrawText("Start", screenWidth / 2 + 400, screenHeight / 2 + 5, 50, WHITE);
         if (isMouseOverButtonStart && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
             game();
         }
 
         bool isMouseOverButtonRules = CheckCollisionPointRec(mousePosition, rulesButton);
 
-        DrawRectangleRec(rulesButton, (isMouseOverButtonRules ? ORANGE : GRAY));
-        DrawText("Rules", screenWidth / 2 + 360, screenHeight / 2 - 25, 50, BLACK);
+        DrawRectangleRec(rulesButton, (isMouseOverButtonRules ? PURPLE : GRAY));
+        DrawText("Rules", screenWidth / 2 + 400, screenHeight / 2 + 157, 50, WHITE);
         if (isMouseOverButtonRules && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
             rules();
         }
 
         bool isMouseOverButtonExit = CheckCollisionPointRec(mousePosition, exitButton);
 
-        DrawRectangleRec(exitButton, (isMouseOverButtonExit ? ORANGE : GRAY));
-        DrawText("Exit", screenWidth / 2 + 25, screenHeight / 2 + 400, 50, BLACK);
+        DrawRectangleRec(exitButton, (isMouseOverButtonExit ? PURPLE : GRAY));
+        DrawText("Exit", screenWidth / 2 + 425, screenHeight / 2 + 315, 50, WHITE);
         if (isMouseOverButtonExit && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
             exitGame = true;
         }
