1e5be0a72d9ff66153191093112a1aa17bc91275
ZVYankova22 <ZVYankova22@codingburgas.bg>
2024-02-23 20:25:06 +0200

    Design lost game menu

--- game/Project/gameLost.cpp
+++ game/Project/gameLost.cpp
@@ -7,6 +7,8 @@ void gameLost()
 	const int screenHeight = 975;
 	const int screenWidth = 1920;
 
+	Texture2D sadAstronaut = LoadTexture("../images/sadAstronaut.png");
+
 	Rectangle tryAgain = { GetScreenWidth() / 2 - 600, GetScreenHeight() / 2 - 10, 300, 90 };
 	Rectangle exitButton = { GetScreenWidth() / 2 + 250, GetScreenHeight() / 2 - 10, 300, 90 };
 	
@@ -20,13 +22,14 @@ void gameLost()
 
 		BeginDrawing();
 
-		DrawText("Game over! You lost!", 580, 50, 80, WHITE);
-		ClearBackground(DARKBLUE);
+		ClearBackground({ 0, 1, 41, 255 });
+		DrawTexture(sadAstronaut, 700, 400, RAYWHITE);
+		DrawText("Game over! You lost!", 580, 150, 80, WHITE);
 
 		bool isMouseOverTryAgain = CheckCollisionPointRec(mousePosition, tryAgain);
 
-		DrawRectangleRec(tryAgain, (isMouseOverTryAgain ? ORANGE : GRAY));
-		DrawText("Try Again", screenWidth / 2 - 575, screenHeight / 2 + 10, 50, BLACK);
+		DrawRectangleRec(tryAgain, (isMouseOverTryAgain ? RED : PURPLE));
+		DrawText("Try Again", screenWidth / 2 - 575, screenHeight / 2 + 10, 50, WHITE);
 		if (isMouseOverTryAgain && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
 		{
 			game();
@@ -34,8 +37,8 @@ void gameLost()
 
 		bool isMouseOverExitGame = CheckCollisionPointRec(mousePosition, exitButton);
 
-		DrawRectangleRec(exitButton, (isMouseOverExitGame ? ORANGE : GRAY));
-		DrawText("Exit Game", screenWidth / 2 + 275, screenHeight / 2 + 10, 50, BLACK);
+		DrawRectangleRec(exitButton, (isMouseOverExitGame ? RED : PURPLE));
+		DrawText("Exit Game", screenWidth / 2 + 275, screenHeight / 2 + 10, 50, WHITE);
 		if (isMouseOverExitGame && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
 		{
 			CloseWindow();
